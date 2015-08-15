d526ec194873f43d578968deed27bca2cdf15b79
laras126 <laras126@gmail.com>
2015-08-15 14:41:31 -0400

    change sidebar name

--- lib/widgets.php
+++ lib/widgets.php
@@ -3,13 +3,13 @@
 // Register Sidebars
 
 $class_sidebar = array(
-	'id'            => 'class_sidebar',
-	'class'         => 'sidebar--class',
-	'name'          => __( 'Class Page Sidebar', 'tbx' ),
-	'description'   => __( 'Widgets to show up on the sidebar of class pages.', 'tbx' ),
-	'before_title'  => '<h4 class="title--widget">',
+	'id'            => 'main_sidebar',
+	'class'         => 'sidebar--main',
+	'name'          => __( 'Main Sidebar', 'tbx' ),
+	'description'   => __( 'Widgets to show up on the sidebar of most pages.', 'tbx' ),
+	'before_title'  => '<h4 class="widget__title">',
 	'after_title'   => '</h4>',
-	'before_widget' => '<li class="widget">',
+	'before_widget' => '<li class="list__item--widget">',
 );
 register_sidebar( $class_sidebar );
 
