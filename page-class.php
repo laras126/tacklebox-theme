22cad07d32585e2fd443ee1ece3edf7500571dd6
laras126 <laras126@gmail.com>
2015-11-19 20:37:35 -0500

    remove comment

--- page-class.php
+++ page-class.php
@@ -5,8 +5,7 @@
  * Description: A Page Template for classes that shows associated lessons.
  */
 
-
-$term_id = get_field('class_taxonomy');
+$term_id = get_field('class_taxonomy'); // Get ACF field
 $lesson_args = array(
 		'post_type' => 'lesson',
 		'tax_query' => array(
@@ -24,6 +23,5 @@ $post = new TimberPost();
 $context['post'] = $post;
 $context['c_user'] = new TimberUser();
 $context['lessons'] = Timber::get_posts($lesson_args);
-// $context['sidebar'] = Timber::get_widgets('main_sidebar');
 
 Timber::render('pages/page-class.twig', $context);
