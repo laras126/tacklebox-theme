da24d4c65824735e70af725e7f28ef2682e4d9d5
laras126 <laras126@gmail.com>
2016-03-19 13:42:29 -0400

    add p2p args to lesson

--- single-lesson.php
+++ single-lesson.php
@@ -23,6 +23,13 @@ $lesson_args = array(
 	    )
 	);
 
+$screencast_args = array(
+	'connected_type' => 'sc_to_lesson',
+	'connected_items' => get_queried_object(),
+	'nopaging' => true,
+	'suppress_filters' => false,
+	'orderby' => 'menu_order'
+);
 
 $user_id = get_current_user_id();
 
@@ -35,6 +42,7 @@ $context['post'] = $post;
 $context['user'] = $user_id;
 
 $context['related_lessons'] = Timber::get_posts($lesson_args);
+$context['screencasts'] = Timber::get_posts($screencast_args);
 
 if (post_password_required($post->ID)){
 	Timber::render('single-password.twig', $context);
